160b25ad49a3ef6a19a4111e1cfd6ebcdb3fb6be
Sharon Fediuk <sharonfediuk@Sharons-MacBook-Pro.local>
2021-03-03 14:26:24 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import  java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String repretword[] = {"ABDA", "ABBACDA", "ABCACBEDD", "AB"};
+        for (int i = 0; i < repretword.length; i++) {
+            char character = repretword[i].charAt(i);
+            int firstIndex = repretword[i].indexOf(character);
+            int lastIndex = repretword[i].lastIndexOf(character);
+            if (firstIndex != lastIndex) {
+                System.out.println("The character '" + character + "' is repeated");
+                break;
+            }
+         int arr[]={1,6,5,8,4,3};
+           Arrays.sort(arr);
+            System.out.println(arr.length-1);
+            System.out.println(arr.length-2 );
+
+        }
+    }
+}
